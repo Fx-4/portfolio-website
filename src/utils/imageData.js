ca2b466f40d04df70144785df55f4e97405b7e6e
kal <Haikalhelmy13@Gmail.com>
2025-11-13 11:31:49 +0800

    Implement code changes to enhance functionality and improve performance

--- src/utils/imageData.js
+++ src/utils/imageData.js
@@ -2,6 +2,9 @@
 import image1 from '../assets/images/Smartlearn.png';
 import image2 from '../assets/images/HistoricalJogja.png';       
 import image3 from '../assets/images/SecondLife.png';
+import image4 from '../assets/images/Nutrikids.png';  
+import image5 from '../assets/images/veriflair.png';
+import image6 from '../assets/images/Tenang.in.png';
 
 
 export const focusCardsData = [
@@ -12,10 +15,32 @@ export const focusCardsData = [
     },
     {
       src: image2, // Update with your actual image paths
-      title: "Historical Jogja"
+      title: "Historical Jogja",
+      url:"https://github.com/Fx-4/Historical-Jogja"  
     },
     {
       src: image3, // Update with your actual image
-      title: "Second Life"
+      title: "Second Life",
+      url:"https://github.com/Fx-4/Sulustara"  
+    
+    },
+        {
+      src: image4, // Update with your actual image
+      title: "Nutrikids",
+      url:"https://github.com/Fx-4/Nutrikids"  
+    
+    },
+        {
+      src: image5, // Update with your actual image
+      title: "Veriflair",
+      url:"https://github.com/Wibson27/VeriFlair"  
+    
+    }
+    ,
+        {
+      src: image6, // Update with your actual image
+      title: "Tenang.in",
+      url:"https://github.com/Wibson27/Tenang.in"  
+    
     }
   ];
